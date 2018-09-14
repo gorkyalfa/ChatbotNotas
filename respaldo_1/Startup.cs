386458a7df6e29ae887e2d5f381974312867ddc2
Miguel Alejandro <42849616+miguelyepez1995@users.noreply.github.com>
2018-09-14 10:34:14 -0500

    Add files via upload

---
+++ respaldo_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(chatbots2.Startup))]
+namespace chatbots2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
