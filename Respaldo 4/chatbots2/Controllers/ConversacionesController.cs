a491071daee16c95163975eadcd6d8b82da27b2c
Jean42Medina <40485748+Jean42Medina@users.noreply.github.com>
2018-09-26 11:00:59 -0500

    Add files via upload

---
+++ Respaldo 4/chatbots2/Controllers/ConversacionesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace chatbots2.Controllers
+{
+    public class ConversacionesController : Controller
+    {
+        // GET: Conversaciones
+        public ActionResult Index()
+        {
+            return View();
+        }
+              
+  
+        // POST: Conversaciones/Index
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            // todo: la pregunta que llegue enviar al luis
+            // todo: la respuesta del luis procesar
+            // todo: lo procesado enviar a notas(si corresponde)
+            // todo: lo devuelto por notas dibujar en la pagina
+            return View();
+        }   
+        public ActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
---
+++ Respaldo 4/chatbots2/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+
+
+
+[assembly: OwinStartupAttribute(typeof(chatbots2.Startup))]
+namespace chatbots2
+{
+    public partial class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            //Any connection or hub wire up and configuration should go here
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
